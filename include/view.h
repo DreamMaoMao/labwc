e7c3947b9b076e8fe8274f23f74f3e5e540619f1
DreamMaoMao <2523610504@qq.com>
2025-06-05 19:33:12 +0800

    feat: add left-occupied and right-occupied to workspaces_find

--- include/view.h
+++ include/view.h
@@ -340,6 +340,9 @@ enum lab_view_criteria {
 	/* Negative criteria */
 	LAB_VIEW_CRITERIA_NO_ALWAYS_ON_TOP        = 1 << 6,
 	LAB_VIEW_CRITERIA_NO_SKIP_WINDOW_SWITCHER = 1 << 7,
+
+	/* Exclude views that are omnipresent */
+	LAB_VIEW_CRITERIA_NO_OMNIPRESENT = 1 << 8,
 };
 
 /**
